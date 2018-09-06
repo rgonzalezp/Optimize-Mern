6b2a8b9f5a488dbd89d2d329c8704365480c8071
Federico Gonzalez <rgonzalezp1115@gmail.com>
2018-09-06 00:46:15 -0500

    Added authentication and personal navigation with oktaClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
+import { Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
+import Navigation from './components/shared/Navigation';
+import HomePage from './components/home/HomePage';
+import RegistrationForm from './components/auth/RegistrationForm';
+import config from './configfile';
+import LoginPage from './components/auth/LoginPage';
+import ProfilePage from './components/auth/ProfilePage';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -9,6 +17,17 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar/>
+        <main>
+          <Route path="/" exact component={HomePage} />
+          <Route
+            path="/login"
+            render={() => <LoginPage baseUrl={config.url} />}
+          />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <Route path="/register" component={RegistrationForm} />
+          <SecureRoute path="/profile" component={ProfilePage} />
+        </main>
+        <Navigation />
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
+import config from './configfile';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
+ReactDOM.render(
+	 <Router>
+    <Security
+      issuer={config.issuer}
+      client_id={config.client_id}
+      redirect_uri={config.redirect_uri}
+      onAuthRequired={onAuthRequired}>
+      <App />
+    </Security>
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
