35b21d58d21bd86f48be92851a7d9317f821e600
Federico Gonzalez <rgonzalezp1115@gmail.com>
2018-09-16 15:56:59 -0500

    fixing redirection

--- client/src/configfile.js
+++ client/src/configfile.js
@@ -1,1 +1,1 @@
-export default{ url: 'https://dev-767504.oktapreview.com/', issuer: 'https://dev-767504.oktapreview.com/oauth2/default', redirect_uri: window.location.origin + '/implicit/callback', client_id: '0oag5cijq9NAMLKqZ0h7'};
+export default{ url: 'https://dev-767504.oktapreview.com/', issuer: 'https://dev-767504.oktapreview.com/oauth2/default', redirect_uri: 'https://optimizelocker.herokuapp.com/', client_id: '0oag5cijq9NAMLKqZ0h7'};
